933c5138309bf25515554347a27dd0ca282e2c62
unknown <adrianmolina39@gmail.com>
2022-01-05 00:42:38 -0400

    Avancving

--- src/components/skill/Skill.js
+++ src/components/skill/Skill.js
@@ -1,18 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Button, Container } from 'react-bootstrap';
+import { Card, Button, Container, InputGroup, FormControl } from 'react-bootstrap';
 import axios from "../../config/axios";
 import NavbarP from '../navbarP/navbarP';
-import agilityIcon from '../../assets/img/agility.png'
+import './skill.css'
+
 
 const defaultState = {
     skills: [],
-    agility: [], attack: [], construction: [], cooking: [],
-    crafting: [], defence: [], farming: [], firemaking: [],
-    fishing: [], fletching: [], herblore: [], hitpoints: [],
-    hunter: [], magic: [], mining: [], overall: [],
-    prayer: [], runecraft: [], smithing: [], strength: [],
-    thieving: [], woodcutting: [], slayer: [], ranged: [],
-    skillsCard: [], arrayHab: [], arrayNames: [],
+    rsn: '',
+    enterRsn: 0
 
 };
 
@@ -20,317 +16,122 @@ const defaultState = {
 
 const Skill = () => {
 
-    
+
 
     const [state, setState] = useState(defaultState);
 
     useEffect(function () {
 
 
-        axios.get('/skills/andresjvr13')
+        axios.get(`/skills/${state.rsn}`)
             .then((res) => {
                 if (res) {
-                    
 
-                    // let pruebaa = [(res.data.skills.agility)];
-                    //  console.log( pruebaa   );
 
-                    // pruebaa.map(dao=>{
-                    //     console.log(dao);
-                    // })
 
-                    // console.log(res.data.data);
 
-                    
+                    console.log(res.data);
 
-                    let arraySkills = [ res.data.data.attack,  res.data.data.construction, res.data.data.cooking, 
-                                        res.data.data.crafting, res.data.data.defence, res.data.data.farming,
-                                        res.data.data.firemaking, res.data.data.fishing, res.data.data.fletching,
-                                        res.data.data.herblore, res.data.data.hitpoints, res.data.data.hunter,
-                                        res.data.data.magic, res.data.data.mining, res.data.data.prayer,
-                                        res.data.data.runecraft, res.data.data.smithing, res.data.data.strength,
-                                        res.data.data.thieving, res.data.data.woodcutting, res.data.data.slayer,
-                                        res.data.data.ranged,
-                                      ]
 
                     let arrayNames = ['attack', 'agility', 'ranged'];
 
 
 
 
-                    setState({ ...state, skills: arraySkills, agility: res.data.data.agility, arrayNames: arrayNames
-                                 })
-                               
-                
+                    setState({
+                        ...state, skills: res.data.result,
+                    })
+
+
                 }
 
-
-                // attack: res.data.data.attack, construction: res.data.data.construction,
-                // cooking: res.data.data.cooking,
-                // crafting: res.data.data.crafting, defence: res.data.data.defence,
-                //  farming: res.data.data.farming, firemaking: res.data.data.firemaking,
-                // fishing: res.data.data.fishing, 
-                //  herblore: res.data.data.herblore, hitpoints: res.data.data.hitpoints,
-                // hunter: res.data.data.hunter, magic: res.data.data.magic,
-                //  mining: res.data.data.mining, overall: res.data.data.overall,
-                // prayer: res.data.data.prayer, runecraft: res.data.data.runecraft,
-                //  smithing: res.data.data.smithing, strength: res.data.data.strength,
-                // thieving: res.data.data.thieving, woodcutting: res.data.data.woodcutting,
-                //  slayer: res.data.data.slayer, 
-                //  fletching: res.data.data.fletching, ranged: res.data.data.ranged,
 
 
 
             })
             .catch((error) => console.log(error))
 
- 
 
-        
 
-            // for (let i = 0; i < (state.data.skills).length; i++) {
-            //     const element = state.data.skills[i];
 
-            //     console.log(element);
-                
-            // }
+    }, [state.enterRsn])  //state.rsn
 
-        //eslint-disable-next-line
-    }, [])
+    const onInputChange = e => {
+
+        const isValid = e.target.validity.valid;
+
+
+        if (isValid === true) {
+            setState({ ...state, [e.target.name]: e.target.value });
+
+        } else {
+            console.log(isValid);
+
+        }
+
+    }
+
+    const onEnterRsn = () => {
+
+            let aux = state.enterRsn;
+            let vali = aux +1;
+            console.log(vali);
+
+            setState({ ...state, enterRsn:vali })
+
+
+
+    }
+
+    const UpperCase = (cadena) => {
+        const str = cadena;
+        const str2 = str.charAt(0).toUpperCase() + str.slice(1);
+        return str2;
+
+    }
 
     return (
         <>
             <NavbarP />
 
+            <InputGroup className="mb-3">
+                <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                <FormControl
+                    placeholder="Username"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                    name="rsn" 
+                    onChange={onInputChange}
+                />
+            </InputGroup>
+            <Button className='primary' onClick={onEnterRsn}> Hola </Button>
 
-            {/* <Card style={{ width: '10rem', height: '15rem' }}>
+            <div className='cards-skill'>
 
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card> */}
+                {state.skills.map((datos) => {
 
-            {state.skills.map( (datos) =>{
+                    if (datos[0] == 'overall') {
+                        
+                    } else {
 
-              
-                return (
-                    <Card style={{ width: '10rem', height: '15rem' }}>
+                        let image = require(`../../assets/img/${datos[0]}.png`)
+                        return (
+                            <Card className='card-skill' key={datos[0]} style={{ width: '10rem', height: '15rem' }}>
 
-                    <Card.Body>
-                        <Card.Title className='text-center'>Agility</Card.Title>
-                        <Card.Text className='text-center card-skill'>
-                            ({datos.level})
-                        </Card.Text>
-                        <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                    </Card.Body>
-                </Card>
-                  )
-            })
+                                <Card.Body>
+                                    <Card.Title className='text-center'>{UpperCase(datos[0])}</Card.Title>
+                                    <Card.Text className='text-center card-skill'>
+                                        ({datos[1].level})
+                                    </Card.Text>
+                                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={image} />
+                                </Card.Body>
+                            </Card>
+                        )
+                    }
+                })
+                }
 
-            }
-
-
-
-            {/* <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card>
-
-            <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card>
-
-            <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '10rem', height: '15rem' }}>
-
-                <Card.Body>
-                    <Card.Title className='text-center'>Agility</Card.Title>
-                    <Card.Text className='text-center card-skill'>
-                        ({state.agility.level})
-                    </Card.Text>
-                    <Card.Img className='img-thumbnail' style={{ height: '50%' }} variant="bottom" src={agilityIcon} />
-                </Card.Body>
-            </Card> */}
-
-
-
-
+            </div>
 
 
         </>
